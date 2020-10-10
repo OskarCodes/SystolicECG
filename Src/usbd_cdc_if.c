369451b994dc61f293cb9fffc5efb9eb66e70126
Oskar Programmes <43321244+OskarProgrammes@users.noreply.github.com>
2020-10-10 13:30:43 +0100

    Update usbd_cdc_if.c
    Added comments for USB CDC functions

--- Src/usbd_cdc_if.c
+++ Src/usbd_cdc_if.c
@@ -263,9 +263,11 @@ static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
 static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
 {
   /* USER CODE BEGIN 6 */
+	//Recieving data function
   USBD_CDC_SetRxBuffer(&hUsbDeviceFS, &Buf[0]);
   USBD_CDC_ReceivePacket(&hUsbDeviceFS);
-	CDC_RecieveCallBack(Buf, Len[0]);
+	//Call the callback function
+  CDC_RecieveCallBack(Buf, Len[0]);
   return (USBD_OK);
   /* USER CODE END 6 */
 }
@@ -285,11 +287,13 @@ uint8_t CDC_Transmit_FS(uint8_t* Buf, uint16_t Len)
 {
   uint8_t result = USBD_OK;
   /* USER CODE BEGIN 7 */
+	//Transmitting function
   USBD_CDC_HandleTypeDef *hcdc = (USBD_CDC_HandleTypeDef*)hUsbDeviceFS.pClassData;
-  if (hcdc->TxState != 0){
+  if (hcdc->TxState != 0) {
     return USBD_BUSY;
   }
   USBD_CDC_SetTxBuffer(&hUsbDeviceFS, Buf, Len);
+	//Send data
   result = USBD_CDC_TransmitPacket(&hUsbDeviceFS);
   /* USER CODE END 7 */
   return result;
